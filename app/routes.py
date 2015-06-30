73f54f60f14c79c9d9918f5cc40ff78caad04309
Michael McHolm <mcholmm@gmail.com>
2015-06-29 20:56:22 -0400

    app category basic crud completed

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, jsonify
+from flask import render_template, jsonify, request
 from app import app
 from app.models import Category
+from app import db
 
 @app.route('/',methods=['GET'])
 @app.route('/index',methods=['GET'])
@@ -12,15 +13,56 @@ def categories():
     return render_template('categories.html')
 
 @app.route('/api/categories',methods=['GET'])
-def rest_categories():
-    Categories = Category.query.all()
+def rest_get_all_categories():
+    Categories = Category.query.order_by(Category.payee).all()
     if len(Categories) > 0:
         categories = [c.serialize() for c in Categories]
     else:
         categories = []
     response = jsonify(categories=categories,
                        status='success')
-    response.status_code = 200;
-
+    response.status_code = 200
     return response
 
+@app.route('/api/category/<category_id>',methods=['GET'])
+def rest_get_category(category_id):
+    category = Category.query.get(category_id)
+    if category:
+        response = jsonify(category=category.serialize(),
+                           status='success')
+        response.status_code = 200
+    else:
+        response = jsonify(status='error')
+        response.status_code = 404
+    return response
+
+@app.route('/api/category/<category_id>',methods=['PUT'])
+def rest_update_category(category_id):
+    category_update = request.json
+    category = Category.query.get(category_id)
+    category.payee = category_update['payee']
+    category.category = category_update['category']
+    category.regex = category_update['regex']
+    db.session.commit()
+    response = jsonify(status='success')
+    response.status_code = 200
+    return response
+
+@app.route('/api/category',methods=['POST'])
+def rest_create_category():
+    category_update = request.json
+    category = Category(category_update['payee'],category_update['category'],category_update['regex'])
+    db.session.add(category)
+    db.session.commit()
+    response = jsonify(status='success')
+    response.status_code = 200
+    return response
+
+@app.route('/api/category/<category_id>',methods=['DELETE'])
+def rest_delete_category(category_id):
+    category = Category.query.get(category_id)
+    db.session.delete(category)
+    db.session.commit()
+    response = jsonify(status='success')
+    response.status_code = 200
+    return response
--- app/static/js/category.js
+++ app/static/js/category.js
@@ -1,5 +1,84 @@
-var app = angular.module('CategoryApp',[]);
-app.controller('CategoryListCtrl',function($scope,$http) {
-  $http.get("/api/categories")
-        .success(function(response) { $scope.categories = response.categories; });
+var app = angular.module('CategoryApp',['ngRoute']);
+app.service('CategoryService',function ($http,$location) {
+  this.getCategories = function () {
+    return $http.get("/api/categories")
+                .then(function(response) {
+                  return response.data;
+                });
+  }
+  this.getCategory = function(category_id) {
+    return $http.get("/api/category/" + category_id)
+                .then(function(response) {
+                  return response.data;
+                },function (response) {
+                  $location.path("/");
+                });
+  }
+  this.updateCategory = function(category) {
+    return $http.put("/api/category/" + category.id,category)
+                .then(function(response) {
+                  $location.path("/");
+                },function (response) {
+                  $location.path("/");
+                });
+  }
+  this.createCategory = function(category) {
+    return $http.post("/api/category",category)
+                .then(function(response) {
+                  $location.path("/");
+                },function (response) {
+                  $location.path("/");
+                });
+  }
+  this.deleteCategory = function(category_id) {
+    return $http.delete("/api/category/" + category_id)
+                .then(function(response) {
+                  $location.path("/deleted");
+                },function (response) {
+                  $location.path("/deleted");
+                });
+  }
 });
+app.controller('CategoryListCtrl',function($scope,CategoryService) {
+  CategoryService.getCategories().then(function (response) {
+          $scope.categories = response.categories;
+        });
+
+  $scope.deleteCategory = function(category_id) {
+    CategoryService.deleteCategory(category_id);
+  }
+});
+app.controller('CategoryEditFormCtrl',function($scope,$location,CategoryService,category) {
+  $scope.category = category.category;
+  $scope.cancelForm = function() {
+    $location.path("/");
+  }
+  $scope.saveCategory = function(category) {
+    CategoryService.updateCategory(category);
+  }
+});
+app.controller('CategoryAddFormCtrl',function($scope,$location,CategoryService) {
+  $scope.cancelForm = function() {
+    $location.path("/");
+  }
+  $scope.saveCategory = function(category) {
+    CategoryService.createCategory(category);
+  }
+});
+app.config(['$routeProvider',function($routeProvider) {
+$routeProvider
+.when('/', {
+        templateUrl: 'static/categories/list.html',
+        controller: 'CategoryListCtrl'})
+.when('/add-category', {
+        templateUrl: 'static/categories/form.html',
+        controller: 'CategoryAddFormCtrl'})
+.when('/edit-category/:category_id', {
+        templateUrl: 'static/categories/form.html',
+        controller: 'CategoryEditFormCtrl',
+        resolve: {
+          category: function(CategoryService,$route) {
+            return CategoryService.getCategory($route.current.params.category_id);
+        }}})
+.otherwise({redirectTo:'/'});
+}]);
